f528985dd775db28fd8bd7b57edfe6efb4e00736
Henrik (Mauritz) Johnson <spam@henrik.org>
2019-04-26 22:42:46 -0700

    Added support for Authorize.Net payment processor. Improved deployment to reduce deployment package to only include needed code

--- src/payments.js
+++ src/payments.js
@@ -1,6 +1,10 @@
 const SquareConnect = require('square-connect');
 const https = require('https');
 
+var ApiContracts = require('authorizenet').APIContracts;
+var ApiControllers = require('authorizenet').APIControllers;
+var SDKConstants = require('authorizenet').Constants;
+
 const common = require("./common.js");
 const sendMessage = require('./sendmessage');
 
@@ -10,12 +14,17 @@ const BTC_ADDRESS = "3382BMuNvJuzMMViccaS4NwduCmt4YYLLo";
 const BTC_AMOUNT = 100000; // 0.001 BTC
 const BTC_AMOUNT_DISPLAY = BTC_AMOUNT / 100000000;
 const ETH_AMOUNT_DISPLAY = ETH_AMOUNT / 1e18;
-const USD_AMOUNT = "1.00"; // 1 USD
+const USD_AMOUNT = 1.0; // 1 USD
+const USD_AMOUNT_DISPLAY = USD_AMOUNT; // 1 USD
 const DOUBLE_SPEND_PERIOD = 1000 * 60; // How long we allow to reuse a token
 
 var squareClient;
 var squareApplicationId;
 var squareLocationId;
+
+var authorizeNetAuth = new ApiContracts.MerchantAuthenticationType();
+authorizeNetAuth.setName(process.env.AUTHORIZE_NET_LOGIN_KEY);
+authorizeNetAuth.setTransactionKey(process.env.AUTHORIZE_NET_TRANSACTION_KEY);
 
 function getSquareLocationId() {
     if (!squareLocationId) {
@@ -154,41 +163,124 @@ function validateTransaction(secretId, type, transaction, address, amount, callb
     });
 }
 
+function successfulPayment(secretId, type, email, transactionId, callback) {
+    sendMessage.send('RECEIPT', email, USD_AMOUNT, transactionId);
+
+    var key = "transactions/" + type + "/" + transactionId + ".json.gz";
+
+    common.putS3File(key, JSON.stringify({
+        timestamp: Date.now(),
+        secretId: secretId,
+        email: email
+    }), null, function (err, data) {
+        if (err) {
+            common.log("WARN", "Failed to save transaction protection for " + key + ": " + err);
+        }
+        callback(null, "Success");
+    });
+}
+
 function validatePayment(secretId, type, token, callback) {
     if (type == "USD") {
         try {
             var data = JSON.parse(token);
 
-            var body = new SquareConnect.ChargeRequest();
-            body.card_nonce = data.nonce;
-            body.buyer_email_address = data.email;
-            body.note = 'Your Shared Secret';
-            body.idempotency_key = data.nonce;
-            body.amount_money = {
-                amount: USD_AMOUNT * 100,
-                currency: 'USD'
-            };
+            if (data.nonce) {
+                var body = new SquareConnect.ChargeRequest();
+                body.card_nonce = data.nonce;
+                body.buyer_email_address = data.email;
+                body.note = 'Your Shared Secret';
+                body.idempotency_key = data.nonce;
+                body.amount_money = {
+                    amount: USD_AMOUNT * 100,
+                    currency: 'USD'
+                };
 
-            getSquareClient().charge(getSquareLocationId(), body).then(function(response) {
-                sendMessage.send('RECEIPT', data.email, USD_AMOUNT, response.transaction.id);
-
-                var key = "transactions/" + type + "/" + response.transaction.id + ".json.gz";
-
-                common.putS3File(key, JSON.stringify({
-                    timestamp: Date.now(),
-                    secretId: secretId,
-                    email: data.email
-                }), null, function(err, data) {
-                    if (err) {
-                        common.log("WARN", "Failed to save double spend protection for " + key + ": " + err);
-                        callback(null, "Success")
-                    } else {
-                        callback(null, "Success");
-                    }
+                getSquareClient().charge(getSquareLocationId(), body).then(function (response) {
+                    successfulPayment(secretId, type, data.email, response.transaction.id, callback);
+                }, function (error) {
+                    callback(error);
                 });
-            }, function(error) {
-                callback(error);
-            });
+            } else if (data.number) {
+                var creditCard = new ApiContracts.CreditCardType();
+                creditCard.setCardNumber(data.number);
+                creditCard.setExpirationDate(data.exp);
+                creditCard.setCardCode(data.code);
+
+                var paymentType = new ApiContracts.PaymentType();
+                paymentType.setCreditCard(creditCard);
+
+                var orderDetails = new ApiContracts.OrderType();
+                orderDetails.setInvoiceNumber(secretId.substr(0, 20));
+                orderDetails.setDescription('Your Shared Secret Information Submission');
+
+                var billTo = new ApiContracts.CustomerAddressType();
+                billTo.setZip(data.zip);
+
+                var customer = new ApiContracts.CustomerDataType();
+                customer.setEmail(data.email);
+
+                var lineItem_id1 = new ApiContracts.LineItemType();
+                lineItem_id1.setItemId('1');
+                lineItem_id1.setName('Your Shared Secret');
+                lineItem_id1.setDescription('Information Submission');
+                lineItem_id1.setQuantity('1');
+                lineItem_id1.setUnitPrice(1);
+
+                var lineItemList = [];
+                lineItemList.push(lineItem_id1);
+
+                var lineItems = new ApiContracts.ArrayOfLineItem();
+                lineItems.setLineItem(lineItemList);
+
+                var transactionRequestType = new ApiContracts.TransactionRequestType();
+                transactionRequestType.setTransactionType(ApiContracts.TransactionTypeEnum.AUTHCAPTURETRANSACTION);
+                transactionRequestType.setPayment(paymentType);
+                transactionRequestType.setAmount(USD_AMOUNT);
+                transactionRequestType.setLineItems(lineItems);
+                transactionRequestType.setOrder(orderDetails);
+                transactionRequestType.setBillTo(billTo);
+                transactionRequestType.setCustomer(customer);
+
+                var createRequest = new ApiContracts.CreateTransactionRequest();
+                createRequest.setMerchantAuthentication(authorizeNetAuth);
+                createRequest.setTransactionRequest(transactionRequestType);
+
+                var ctrl = new ApiControllers.CreateTransactionController(createRequest.getJSON());
+
+                if (process.env.AUTHORIZE_NET_ENV === 'PROD')
+                    ctrl.setEnvironment(SDKConstants.endpoint.production);
+
+                ctrl.execute(function () {
+
+                    var apiResponse = ctrl.getResponse();
+
+                    var response = new ApiContracts.CreateTransactionResponse(apiResponse);
+
+                    if (response != null) {
+                        if (response.getMessages().getResultCode() == ApiContracts.MessageTypeEnum.OK) {
+                            if (response.getTransactionResponse().getMessages() != null) {
+                                successfulPayment(secretId, type, data.email, response.getTransactionResponse().getTransId(), callback);
+                                return;
+                            }
+                        }
+                        console.log('Failed Transaction.');
+                        if (response.getTransactionResponse() != null && response.getTransactionResponse().getErrors() != null) {
+                            console.log('Error Code: ' + response.getTransactionResponse().getErrors().getError()[0].getErrorCode());
+                            console.log('Error message: ' + response.getTransactionResponse().getErrors().getError()[0].getErrorText());
+                        } else if (response.getMessages() != null) {
+                            console.log('Error Code: ' + response.getMessages().getMessage()[0].getCode());
+                            console.log('Error message: ' + response.getMessages().getMessage()[0].getText());
+                        }
+                    } else {
+                        console.log('Null Response for payment.');
+                    }
+
+                    callback("Payment processing failed");
+                });
+            } else {
+                callback("Invalid payment information");
+            }
         } catch (err) {
             callback(err);
         }
@@ -219,7 +311,7 @@ function validatePayment(secretId, type, token, callback) {
 function paymentDefinition(callback) {
     callback(null, {
         USD: {
-            amount: 1.0,
+            amount: USD_AMOUNT_DISPLAY,
             token: JSON.stringify({
                 applicationId: getSquareApplicationId(),
                 locationId: getSquareLocationId()
